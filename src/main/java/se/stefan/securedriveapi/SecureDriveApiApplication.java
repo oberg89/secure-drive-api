dc0e9afde53cf02d02b2e152811361648eb6c1dc
Stefan Öberg <oberg@hotmail.se>
2026-01-22 12:00:49 +0100

    Initialize Spring Boot project structure with layered architecture

---
+++ src/main/java/se/stefan/securedriveapi/SecureDriveApiApplication.java
@@ -0,0 +1,13 @@
+package se.stefan.securedriveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureDriveApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureDriveApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/stefan/securedriveapi/SecureDriveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.stefan.securedriveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDriveApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
