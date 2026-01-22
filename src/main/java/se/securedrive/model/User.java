17450801303c4308c1d2afb0a3060f5e482b7b79
Stefan Öberg <oberg@hotmail.se>
2026-01-22 14:01:04 +0100

    Add User entity

---
+++ src/main/java/se/securedrive/model/User.java
@@ -0,0 +1,34 @@
+package se.securedrive.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
+    private List<Folder> folders;
+
+    @OneToMany(meppedBy = "owner", cascade = CascadeTpye.ALL)
+    private List<FileEntity> files;
+
+}
