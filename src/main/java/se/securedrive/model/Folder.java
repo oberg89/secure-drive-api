e551e796614a57f86dcd19bc531e00c9b5b37024
Stefan Öberg <oberg@hotmail.se>
2026-01-22 14:25:03 +0100

    Add Folder entity with user ownership

---
+++ src/main/java/se/securedrive/model/Folder.java
@@ -0,0 +1,31 @@
+package se.securedrive.model.;
+
+import  jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User owner;
+
+    @OneToMany(mappedBy = "folder", cascade = CascadeType.ALL)
+    private List<FileEntity> files;
+
+}
